c537836a72de6865a37ded4572ba7981ab436393
nhussaini <nasrullah_hussaini@yahoo.com>
2021-05-05 19:15:29 +0000

    added input module

--- play.js
+++ play.js
@@ -1,28 +1,10 @@
-const connect = require('./client')
-const stdin = process.stdin;
+
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 
 console.log("Connecting ...");
+
 connect();
 
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  //event listener
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // your code here
-  
-    if (key === '\u0003') {
-      process.exit();
-    }
-  
-};
 setupInput();
